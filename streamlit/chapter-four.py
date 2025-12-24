252b945cc2cfe5a09ba5a38d30e478c7f5a4d0f5
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2025-12-24 13:50:37 +0500

    added chapter 4 for streamlit

--- streamlit/chapter-four.py
+++ streamlit/chapter-four.py
@@ -6,4 +6,11 @@ if file:
     df = pd.read_csv(file)
     st.subheader("Sales Data Preview:")
     st.dataframe(df)
-    
+if file:
+    st.subheader("Summary Stats")
+    st.write(df.describe())
+if file:
+    cities = df["city"].unique()
+    selected_city = st.selectbox("Select City to filter sales data", cities)
+    filtered_data = df[df["city"] == selected_city]
+    st.dataframe(filtered_data)
